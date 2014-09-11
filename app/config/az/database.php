211e431d90ad76b4d68d1b7a7c53b38cb2c2cfce
sngrl <reserved@mail.ru>
2014-09-11 18:32:44 +0400

    +

--- app/config/az/database.php
+++ app/config/az/database.php
@@ -13,7 +13,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'shveinaya-lavka',
+			'database'  => 'plitka',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
