a92a03bc4ada768e569a68bd4413b16191bd1200
sngrl <reserved@mail.ru>
2014-11-11 12:31:54 +0300

    colors

---
+++ app/config/dic/colors.php
@@ -0,0 +1,58 @@
+<?php
+
+return array(
+
+    'fields' => function () {
+
+        return array(
+
+            'css_code' => array(
+                'title' => 'Шестизначный код цвета CSS',
+                'type' => 'text',
+            ),
+        );
+
+    },
+
+
+    'menus' => function($dic, $dicval = NULL) {
+
+    },
+
+
+    'actions' => function($dic, $dicval) {
+    },
+
+
+    'hooks' => array(
+
+        'before_all' => function ($dic) {
+        },
+
+        'before_index' => function ($dic) {
+        },
+
+        'before_index_view' => function ($dic, $dicvals) {
+        },
+
+        'before_create_edit' => function ($dic) {
+        },
+        'before_create' => function ($dic) {
+        },
+        'before_edit' => function ($dic, $dicval) {
+        },
+
+        'before_store_update' => function ($dic) {
+        },
+        'before_store' => function ($dic) {
+        },
+        'before_update' => function ($dic, $dicval) {
+        },
+
+        'before_destroy' => function ($dic, $dicval) {
+        },
+    ),
+
+
+    'seo' => false,
+);
