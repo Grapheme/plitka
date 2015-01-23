cb619651f730a7bedcb07e55e12bc58154233422
sngrl <reserved@mail.ru>
2015-01-23 17:01:00 +0300

    fix

--- app/config/packages/scalia/sphinxsearch/config.php
+++ app/config/packages/scalia/sphinxsearch/config.php
@@ -2,7 +2,7 @@
 
 return array (
 	'host'    => '127.0.0.1',
-	'port'    => 3312,
+	'port'    => 9312,
 	'indexes' => array (
 		'my_index_name' => array ( 'table' => 'keywords', 'column' => 'id' ),
 	)
