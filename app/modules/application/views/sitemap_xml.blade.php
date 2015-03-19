8247b261d72113b2da3c5acb21873abb0d2d36f2
sngrl <reserved@mail.ru>
2015-03-19 16:59:29 +0300

    fix

--- app/modules/application/views/sitemap_xml.blade.php
+++ app/modules/application/views/sitemap_xml.blade.php
@@ -7,9 +7,22 @@
     </url>
     @if (isset($data['pages']) && count($data['pages']))
         @foreach ($data['pages'] as $page)
+            <?
+            if ($page['slug'] == 'index')
+                continue;
+            ?>
             <url>
                 <loc>{{ URL::route('mainpage') }}#!/{{ $page['slug'] }}</loc>
                 <changefreq>weekly</changefreq>
+                <priority>0.6</priority>
+            </url>
+        @endforeach
+    @endif
+    @if (isset($data['collections']) && count($data['collections']))
+        @foreach ($data['collections'] as $collection)
+            <url>
+                <loc>{{ URL::route('mainpage') }}#!/collection/{{ $collection['slug'] }}</loc>
+                <changefreq>daily</changefreq>
                 <priority>0.8</priority>
             </url>
         @endforeach
